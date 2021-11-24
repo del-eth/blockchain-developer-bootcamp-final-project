793663ba3bbc07ab7caf28c589474ab8954e6bf4
Delano Johnson <delano_j@outlook.com>
2021-11-23 17:16:52 -0700

    some events and modifiers

--- contracts/ProofOfTrees.sol
+++ contracts/ProofOfTrees.sol
@@ -5,7 +5,8 @@ contract ProofOfTrees {
     address payable public owner;
 
     //map of unique EXIF hash to determined tree data
-    mapping(string => Tree) trees;
+    //TODO: public necessary?
+    mapping(string => Tree) public trees;
     
     enum TreeState {
         Approved,
@@ -27,5 +28,59 @@ contract ProofOfTrees {
         TreeType tType;
         uint256 lat;
         uint256 long;
+        address payable hippie;
+        address payable curator;
+    }
+
+
+    /*
+     * Events
+     */
+
+    // <LogApproved event: string exifSHA>
+    event LogApproved(string exifSHA);
+
+    // <LogPaid event: string exifSHA>
+    event LogPaid(string exifSHA);
+
+    // <LogRejected event: string exifSHA>
+    event LogRejected(string exifSHA);
+
+    // <LogSubmitted event: string exifSHA>
+    event LogSubmitted(string exifSHA);
+
+    // <LogUnderReview event: string exifSHA>
+    event LogUnderReview(string exifSHA);
+
+    /*
+     * Modifiers
+     */
+    modifier isOwner() {
+        require(
+            msg.sender == owner,
+            "Only the owner can call this function."
+        );
+        _;
+    }
+
+    modifier isHippie(string memory _exifSHA) {
+        require(
+            msg.sender == trees[_exifSHA].hippie,
+            "Only the Hippie can call this function."
+        );
+        _;
+    }
+
+    modifier isCurator(string memory _exifSHA) {
+        require(
+            msg.sender == trees[_exifSHA].curator,
+            "Only the Curator can call this function."
+        );
+        _;
+    }
+
+    modifier verifyCaller(address _address) {
+        require(msg.sender == _address);
+        _;
     }
 }
