efb5d7c352b1fbb62808558bb556dea3492b6936
Delano Johnson <delano_j@outlook.com>
2021-11-26 16:25:22 -0700

    add erc 20 library

--- contracts/ProofOfTrees.sol
+++ contracts/ProofOfTrees.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ProofOfTrees {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ProofOfTrees{
     address public owner;
     uint256 public treeCount;
     uint256 public lastCurator;
